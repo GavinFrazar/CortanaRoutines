6c2ebc48d8f7c6e8db8d9df4617d109d4f7b5990
Myles Adams <myles.n.adams@gmail.com>
2018-04-12 17:29:55 -0700

    Initial weather app work

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
+A simple echo bot for the Microsoft Bot Framework.
 -----------------------------------------------------------------------------*/
 
 var restify = require('restify');
@@ -7,7 +7,7 @@ var builder = require('botbuilder');
 var botbuilder_azure = require("botbuilder-azure");
 
 /*-----------------------
-setting up table storage 
+setting up table storage
 -----------------------*/
 var storage = require('azure-storage');
 var connectionString = "DefaultEndpointsProtocol=https;AccountName=apollostorage2;AccountKey=35ZE7pRvyiDOv5mPNoUcIXQxfpocc4fDLASOe1p1mPIHv1fR2Y6yA7bnhiaoEpRozHphcFiKMR6wZrFFdsAlkQ==;TableEndpoint=https://apollostorage2.table.cosmosdb.azure.com:443/;";
@@ -29,9 +29,9 @@ storageClient.createTableIfNotExists('mytesttable', function (error, result, res
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+   console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
@@ -39,11 +39,11 @@ var connector = new builder.ChatConnector({
     openIdMetadata: process.env.BotOpenIdMetadata
 });
 
-// Listen for messages from users 
+// Listen for messages from users
 server.post('/api/messages', connector.listen());
 
 /*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* Bot Storage: This is a great spot to register the private state storage for your bot.
 * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
 * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
 * ---------------------------------------------------------------------------------------- */
@@ -85,3 +85,43 @@ storageClient.executeBatch('mytesttable', batch, function (error, result, respon
         console.log('Batch completed');
     }
 });
+
+
+bot.dialog('/weather', [
+    function(session) {
+        var userInfo = session.message.entities.find((e) => {
+            return e.type === 'UserInfo';
+        });
+
+        if (userInfo) {
+            var currentLocation = userInfo['CurrentLocation'];
+
+            if (currentLocation)
+            {
+                var lat = currentLocation.Hub.Latitude;
+                var lon = currentLocation.Hub.Longitude;
+
+            }
+        }
+
+        var weather_call = weather_api_uri + 'lat=' + lat + '&lon=' = lon + '&APPID=' + weather_api_appid;
+        fetch(weather_call)
+        .then(res => res.json())
+        .then(json => {
+            main = json.main;
+            var temp_cur = main.temp;
+            var temp_min = main.temp_min;
+            var temp_max = main.temp_max;
+            var wind_speed = json.wind.speed;
+
+            var sunrise_date = new Date(0);
+            var sunset_date = new Date();
+            sunrise_date.setUTCSeconds(json.sys.sunrise);
+            sunset_date.setUTCSeconds(json.sys.sunset);
+
+            console.log(current_temp);
+                // console.log(json);
+            console.log(json.weather[0].description);
+        })
+    }
+])
